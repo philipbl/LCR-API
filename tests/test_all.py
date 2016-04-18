52c539b544259e9d038dbf9e2e706e6558e90212
Philip Lundrigan <philiplundrigan@gmail.com>
2016-04-18 17:31:49 -0600

    Redo tests without betamax
    It doesn't seem to be working for my usage and it is probably better
    anyways. I need to know if the interactions change.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,41 +1,78 @@
-import pytest
 import os
 import lcr
 
 
-@pytest.mark.usefixtures('betamax_session')
 class Test:
-    def setup_method(self, method):
-        self.user = os.environ.get('LDS_USER')
-        self.password = os.environ.get('LDS_PASSWORD')
+    @classmethod
+    def setup_class(cls):
+        user = os.environ.get('LDS_USER')
+        password = os.environ.get('LDS_PASSWORD')
+        cls.cd = lcr.API(user, password)
 
-    def test_birthday(self, betamax_session):
-        cd = lcr.API(self.user, self.password, betamax_session)
-        birthdays = cd.birthday_list(4, 1)
+    def test_birthday(self):
+        birthdays = Test.cd.birthday_list(4, 1)
+
         birthdays = birthdays[0]['birthdays']
+        assert isinstance(birthdays, list)
 
-        assert len(birthdays) == 40
-        assert birthdays[0]['name'] == 'Mita, Jessica Lorene'
+        birthday = birthdays[0]
+        assert isinstance(birthday, dict)
 
-    def test_moveins(self, betamax_session):
-        cd = lcr.API(self.user, self.password, betamax_session)
-        moveins = cd.members_moved_in()
+        for key in ['formattedBirthDate', 'age', 'formattedBirthDateFull',
+                    'spokenName', 'birthDateSort', 'name']:
+            assert key in birthday
 
-        address = '645 E 200 S<br />Apt 11<br />Salt Lake City, Utah 84012'
-        assert len(moveins) == 15
-        assert moveins[0]['address'] == address
+    def test_moveins(self):
+        moveins = Test.cd.members_moved_in()
+        assert isinstance(moveins, list)
 
-    def test_moveouts(self, betamax_session):
-        cd = lcr.API(self.user, self.password, betamax_session)
-        moveouts = cd.members_moved_out()
+        movein = moveins[0]
+        assert isinstance(movein, dict)
 
-        assert len(moveouts) == 12
-        assert moveouts[0]['moveDate'] == '23 Mar 2016'
+        for key in ["formattedMrn", "formattedMoveInDate", "birthDate",
+                    "moveDateOrder", "setApart", "priesthoodType",
+                    "birthDateSort", "householdPhone",
+                    "formattedBirthDateFull", "priesthood", "mrn",
+                    "actualAge", "outOfUnitMember", "unitNumber",
+                    "sustainedDate", "nameOrder", "address", "textAddress",
+                    "hohMrn", "visible", "nonMember", "gender",
+                    "addressUnknown", "priorUnit", "actualAgeInMonths",
+                    "householdEmail", "genderLabelShort"]:
+            assert key in movein
 
-    def test_assignments(self, betamax_session):
-        cd = lcr.API(self.user, self.password, betamax_session)
-        home_teaching = cd.custom_home_and_visiting_teaching()
+    def test_moveouts(self):
+        moveouts = Test.cd.members_moved_out()
+        assert isinstance(moveouts, list)
 
-        assert len(home_teaching['hp']) == 7
-        assert len(home_teaching['eq']) == 6
-        assert len(home_teaching['rs']) == 8
+        moveout = moveouts[0]
+        assert isinstance(moveout, dict)
+
+        for key in ["birthDate", "moveDateOrder", "formattedBirthDate",
+                    "nextUnitName", "moveDateStored", "nextUnitNumber",
+                    "addressUnknown", "nameOrder", "moveDate", "name",
+                    "birthDateStored", "priorUnit"]:
+            assert key in moveout
+
+    def test_assignments(self):
+        home_teaching = Test.cd.custom_home_and_visiting_teaching()
+        assert isinstance(home_teaching, dict)
+
+        hp = home_teaching['hp']
+        assert isinstance(hp, list)
+
+        eq = home_teaching['eq']
+        assert isinstance(eq, list)
+
+        rs = home_teaching['rs']
+        assert isinstance(rs, list)
+
+        families = home_teaching['families']
+        assert isinstance(families, list)
+
+        # Check home teaching
+        # TODO: Finish
+
+        # Check families
+        for key in ["emailAddress", "address", "spouse", "phone", "children",
+                    "headOfHouse", "formattedCoupleName", "homeTaughtByAuxId"]:
+            assert key in families[0]
