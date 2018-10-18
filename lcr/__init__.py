59a17b4ab531817e7f00aa7eedd7d72f86161ff4
Kaleb Olson <kaleb.olson@gmail.com>
2018-10-18 12:50:15 -0600

    Added new API endpoints:
    * Ministering
    * Alternate members-list with different keys
    * Access table for roles

--- lcr/__init__.py
+++ lcr/__init__.py
@@ -120,3 +120,39 @@ class API():
 
         result = self._make_request(request)
         return result.json()
+
+
+    def members_alt(self):
+        _LOGGER.info("Getting member list")
+        request = {'url': 'https://{}/services/umlu/report/member-list'.format(LCR_DOMAIN),
+                   'params': {'lang': 'eng',
+                              'unitNumber': self.unit_number}}
+
+        result = self._make_request(request)
+        return result.json()
+
+
+    def ministering(self):
+        """
+        API parameters known to be accepted are lang type unitNumber and quarter.
+        """
+        _LOGGER.info("Getting ministering data")
+        request = {'url': 'https://{}/services/umlu/v1/ministering/data-full'.format(LCR_DOMAIN),
+                   'params': {'lang': 'eng',
+                              'unitNumber': self.unit_number}}
+
+        result = self._make_request(request)
+        return result.json()
+
+
+    def access_table(self):
+        """
+        Once the users role id is known this table could be checked to selectively enable or disable methods for API endpoints.
+        """
+        _LOGGER.info("Getting info for data access")
+        request = {'url': 'https://{}/services/access-table'.format(LCR_DOMAIN),
+                   'params': {'lang': 'eng'}}
+
+        result = self._make_request(request)
+        return result.json()
+
